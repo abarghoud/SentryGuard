23649cb92e18ae4c8672f92a2ca7e4959ca6c21d
Anas Barghoud <abarghoud@gmail.com>
2026-06-04 22:46:58 +0200

    refactor: remove initial feature announcement record from migration files

--- apps/api/src/migrations/1774500000000-CreateFeatureAnnouncementsTable.ts
+++ apps/api/src/migrations/1774500000000-CreateFeatureAnnouncementsTable.ts
@@ -12,10 +12,6 @@ export class CreateFeatureAnnouncementsTable1774500000000 implements MigrationIn
         CONSTRAINT "PK_feature_announcements" PRIMARY KEY ("key")
       )`
     );
-
-    await queryRunner.query(
-      `INSERT INTO "feature_announcements" ("key", "is_active") VALUES ('break_in_offensive_response_v1', true)`
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
